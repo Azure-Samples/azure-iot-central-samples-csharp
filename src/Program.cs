fe3532fef922f2f231f21ccdbda2344be186de0e
formulahendry <formulahendry@gmail.com>
2019-10-25 13:23:25 +0800

    Auto generate C# SDK from Swagger

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace IoTCentral
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", "[ACCESS_TOKEN]");
+            var deviceClient = new DevicesClient(httpClient);
+            var result = await deviceClient.ListAsync();
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
